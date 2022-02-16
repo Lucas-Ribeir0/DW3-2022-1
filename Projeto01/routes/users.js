d272facdbb4d8b98634f892f7d67ce99749f88e9
LUCAS ANTONIO RIBEIRO <1091392113016@fatec-srv.local>
2022-02-15 22:09:56 -0300

    15/02

--- Projeto01/routes/users.js
+++ Projeto01/routes/users.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router()
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
     return res.send({message: `Método GET de usuários funcionando corretamente!`})
 });
 
-router.post('/users', (req, res) => {
+router.post('/', (req, res) => {
     return res.send({message: `Método POST de usuários funcionando corretamente!`})
 });
 
 
 // Reconhece /users junto ao app.use? 
 router.post('/create', (req, res) => {
-    return res.send({message: `Método POST de usuários funcionando corretamente!`})
+    return res.send({message: `Usuários criado com sucesso!`})
 });
 
 module.exports = router;
