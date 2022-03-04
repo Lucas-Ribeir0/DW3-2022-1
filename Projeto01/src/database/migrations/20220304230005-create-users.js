f6d781359e3619e3d3e1ca6ea78c842682ea4e37
LUCAS ANTONIO RIBEIRO <1091392113016@fatec-srv.local>
2022-03-04 20:17:59 -0300

    04/03

---
+++ Projeto01/src/database/migrations/20220304230005-create-users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      usr_name: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      usr_email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      usr_password: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }, 
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }, 
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+
+  }
+};
